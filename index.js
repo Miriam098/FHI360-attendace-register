cc90e89acce1157317e7171d727e42b16cae5be5
Miriam Olivier <miriamolivier098@gmail.com>
2021-07-28 14:28:48 +0200

    AGYW attendance register for FHI360

---
+++ index.js
@@ -0,0 +1,21 @@
+function displayDateOne() {
+ document.getElementById("showDate").style.display = 'block';
+}
+function displayDateTwo() {
+    document.getElementById("showDate").style.display = 'none';
+}
+
+function displayDateOne1() {
+ document.getElementById("showDateOne").style.display = 'block';
+}
+function displayDateTwo1() {
+    document.getElementById("showDateOne").style.display = 'none';
+}
+
+function displayDateOne2() {
+ document.getElementById("showDateThree").style.display = 'block';
+}
+function displayDateTwo2() {
+    document.getElementById("showDateThree").style.display = 'none';
+}
+
